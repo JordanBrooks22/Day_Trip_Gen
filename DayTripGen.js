3ba0e3760865a498a152cfce11a84b4beea71409
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-11 13:12:24 -0500

    js file has been rearranged/updated

--- DayTripGen.js
+++ DayTripGen.js
@@ -5,73 +5,76 @@
    let transportation = ["Mustang", "Bike", "Snow-mobile", "Audi"];
    let randomNumber = Math.floor(Math.random() * entertainment.length);
 
-  function destination() {
-    
-    let randomNumber = Math.floor(Math.random() * destinations.length);
-    //once var was declared for math.floor random will only need it once
-    let chosenDestination = destinations[randomNumber];
-    return chosenDestination;
-}
 
-function restaurant() {
-    
-    let randomNumber = Math.floor(Math.random() * restaurant.length);
-    let chosenRestaurant = restaurant[randomNumber];
-    return chosenRestaurant;
-}
-function transportation() {
-    
-    let randomNumber = Math.floor(Math.random() * transportation.length);
-    let chosenTransportation = transportation[randomNumber];
-    return chosenTransportation;
-}
-function entertainment() {
-    let entertainment = ["Hike", "movies", "swim", "dance"];
-  
-    let chosenEntertainment = entertainment[randomNumber];
-    return chosenEntertainment;
+   let userTrip = [ getRandomElement(destinations), getRandomElement(transportation), getRandomElement(restaurant), getRandomElement(activities) ];
+
+   console.log("Here is your trip: \nYou will go to: " + userTrip[0] + "\nYou you will travel by: " + userTrip[1] + "\nYou will eat at: " + userTrip[2] + "\nFor entertainment you will: " + userTrip[3])
 
 
-}
+let userInput = "";
 
-function dayTrip() {
-        let chosenDestination = destination();
-        let chosenRestaurant = restaurant();
-        let chosenTransportation = transportation();
-        let chosenEntertainment = entertainment();
-        let dayTrip = ("You will take a " + chosenTransportation + "to visit" + chosenRestaurant + " in the city of " + )
+   while(true){
+
+    while(true){
+        userInput = prompt("Would you like to change anything about your trip? yes/no:");
+         userInput = userInput.toLowerCase();
+        if(userInput.charAt(0) === "y"){
+            break
+        }
+        else if(userInput.charAt(0) === "n"){
+            break
+        }
+        else{
+
+        }
     }
 
+    if(userInput.charAt(0) === "y"){
+        
+        while (true){
+        userInput = prompt("What would you like to change?: \n1) Destination \n2) Transportation \n3) Restaruant \n4) Entertainment")
+        
+        if(isNaN(userInput)){
+            
+        }
+        else{
+            userInput = parseInt(userInput);
+            break;
+        }
+        }
+
+        if(userInput === 1){
+            userTrip[0] = getRandomElement(destinations);
+        }
+        else if(userInput === 2){
+            userTrip[1] = getRandomElement(transportation);
+        }
+        else if(userInput === 3){
+            userTrip[2] = getRandomElement(restaurant);
+        }
+        else if(userInput === 4){
+            userTrip[3] = getRandomElement(activities);
+        }
+    }
+    else{
+        userInput = prompt("Does your trip look good then? yes/no: ");
+        if(userInput.charAt(0) === "y"){
+            break;
+        }
+        else{
+            
+        }
+    }
+
+    console.log("Here is your trip: \nYou will go to: " + userTrip[0] + "\nYou you will travel by: " + userTrip[1] + "\nYou will eat at: " + userTrip[2] + "\nFor entertainment you will: " + userTrip[3]);
+} 
+
+console.log("Here is your confirmed trip: \nYou will go to: " + userTrip[0] + "\nYou you will travel by: " + userTrip[1] + "\nYou will eat at: " + userTrip[2] + "\nFor entertainment you will: " + userTrip[3]);
 
 
+function getRandomElement(arrayToGrab){
+    let randomElement = arrayToGrab[Math.floor(Math.random() * arrayToGrab.length)];
+
+    return randomElement;
 }
-
-
-
-
-
-
-function choosefirstCheck() {
-let firstCheck = prompt("Different day trip?");
-}
-
-
-if(firstCheck === "yes"){
-    function chooseDestination()
-
-
-
-let completeBook = prompt("Complete booking click here");
-
-}
-
-completeBook
-
-
-
-//if yes then re cycle through function choose destinations
-//if no user wants to confirm day trip is complete if they like each selection
-
-
-
 
